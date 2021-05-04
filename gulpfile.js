8733ffa10016a6e209468b4647759b6c8df2fede
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2021-05-04 12:43:34 +0300

    Добавление скриптов: мобильное меню, Google карта, отправка формы и валидация форм

--- gulpfile.js
+++ gulpfile.js
@@ -49,13 +49,13 @@ const html = () => {
 
 // JS
 
-const scripts = () => {
-  return gulp.src("source/js/script.js")
-    .pipe(terser())
-    .pipe(gulp.dest("build/js"))
-    .pipe(sync.stream());
-}
-exports.scripts = scripts;
+// const scripts = () => {
+//   return gulp.src("source/js/script.js")
+//     .pipe(terser())
+//     .pipe(gulp.dest("build/js"))
+//     .pipe(sync.stream());
+// }
+// exports.scripts = scripts;
 
 // Clean
 
@@ -107,6 +107,7 @@ const copy = (done) => {
     "source/assets/*ico",
     "source/assets/*.json",
     "source/img/**/*.{jpg,png,svg}",
+    "source/js/script.js"
   ], {
     base: "source"
   })
@@ -138,7 +139,7 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/js/*.js", gulp.series("scripts"));
+  // gulp.watch("source/js/*.js", gulp.series("scripts"));
   gulp.watch("source/*.html", gulp.series(html, reload));
 };
 exports.default = gulp.series(styles, server, watcher);
@@ -152,7 +153,7 @@ const build = gulp.series(
   gulp.parallel(
     styles,
     html,
-    scripts,
+    // scripts,
     sprite,
     createWebp
   ),
--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,77 @@
-console.log("Hello");
+const menu = document.querySelector(".menu");
+const menuControl = menu.querySelector(".menu__control");
+const iconClose = menu.querySelector(".menu__close");
+const iconOpen = menu.querySelector(".menu__open");
+const menuNavigation = menu.querySelector(".menu__navigation");
+
+const mobileMenu = () => {
+  const menuToggle = () => {
+    iconClose.classList.toggle("active");
+    iconOpen.classList.toggle("active");
+    menu.classList.toggle("menu--closed");
+
+    menuControl.setAttribute("aria-label", "Закрыть меню");
+    if (!menu.classList.contains("menu--closed")) {
+      menuControl.setAttribute("aria-label", "Открыть меню");
+    }
+  };
+
+  menuControl.addEventListener("click", menuToggle);
+};
+
+mobileMenu();
+
+const sliderControl = document.querySelector(".slider__control");
+if (sliderControl) {
+  const slidePrev = sliderControl.querySelector(".slider__button--prev");
+  const sliderNext = sliderControl.querySelector(".slider__button--next");
+  const pin = sliderControl.querySelector(".range__pin");
+}
+
+const URL = "https://echo.htmlacademy.ru";
+const forms = document.querySelectorAll(".questionnaire__form");
+
+if (forms) {
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Заявка принята",
+    failure: "Что-то пошло не так...",
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector(".status").textContent = message.loading;
+    let res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+    return await res.text();
+  };
+
+  const formHandler = (item) => {
+    let statusMessage = document.createElement("div");
+    statusMessage.classList.add("status");
+    item.appendChild(statusMessage);
+
+    const formData = new FormData(item);
+
+    postData(URL, formData)
+      .then((res) => {
+        console.log(res);
+        statusMessage.textContent = message.success;
+      })
+      .catch(() => (statusMessage.textContent = message.failure))
+      .finally(() => {
+        item.reset();
+        setTimeout(() => {
+          statusMessage.remove();
+        }, 5000);
+      });
+  };
+
+  forms.forEach((item) => {
+    item.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      formHandler(item);
+    });
+  });
+}
